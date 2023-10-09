01ef4556e9932a9640bce43d53d609edaa411be3
billwangwz <3420680@qq.com>
2023-10-09 11:51:53 -0500

    Update vast.sh

--- vast.sh
+++ vast.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xb9EA3C9706FE1Fa41CF21A149856309d34623E10/g' config.conf
 sleep 5
 echo "---------------------------"
 
