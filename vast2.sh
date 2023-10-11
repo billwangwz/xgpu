cc8679427f9fad6ce3f2373c7c650aa7edefabdb
billwangwz <3420680@qq.com>
2023-10-11 15:35:38 -0500

    Update and rename vast8.sh to vast2.sh

--- vast2.sh
+++ vast2.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xb9ea3c9706fe1fa41cf21a149856309d34623e10/g' config.conf
 sleep 5
 echo "---------------------------"
 
@@ -66,26 +66,3 @@ sudo nohup ./xengpuminer -d1 > xengpuminer-1.log 2>&1 &
 sleep 5
 echo "---------------------------"
 
-sudo nohup ./xengpuminer -d2 > xengpuminer-2.log 2>&1 &
-sleep 5
-echo "---------------------------"
-
-sudo nohup ./xengpuminer -d3 > xengpuminer-3.log 2>&1 &
-sleep 5
-echo "---------------------------"
-
-sudo nohup ./xengpuminer -d4 > xengpuminer-4.log 2>&1 &
-sleep 5
-echo "---------------------------"
-
-sudo nohup ./xengpuminer -d5 > xengpuminer-5.log 2>&1 &
-sleep 5
-echo "---------------------------"
-
-sudo nohup ./xengpuminer -d6 > xengpuminer-6.log 2>&1 &
-sleep 5
-echo "---------------------------"
-
-sudo nohup ./xengpuminer -d7 > xengpuminer-7.log 2>&1 &
-sleep 5
-echo "---------------------------"
